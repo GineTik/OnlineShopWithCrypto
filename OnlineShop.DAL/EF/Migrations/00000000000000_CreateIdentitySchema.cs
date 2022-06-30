b7ea274d7ec9b0a21f4e5af527f12ac23366d55e
Artur358 <itgamevenpc@gmail.com>
2022-06-30 13:40:52 +0300

    baza

--- OnlineShop.DAL/EF/Migrations/00000000000000_CreateIdentitySchema.cs
+++ OnlineShop.DAL/EF/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
-namespace OnlineShop.Data.Migrations
+namespace OnlineShop.DAL.EF.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
--- OnlineShop/Startup.cs
+++ OnlineShop/Startup.cs
@@ -7,11 +7,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OnlineShop.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using OnlineShop.DAL.EF;
+
 
 namespace OnlineShop
 {
@@ -31,6 +33,10 @@ namespace OnlineShop
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+            
+
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
